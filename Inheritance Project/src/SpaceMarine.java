1fb6922f20fb86b21c4a3cfb0ba8d340ef576b1d
Jonathan <Jonathan@10.0.0.7>
2024-04-14 15:16:40 -0600

    Added methods and variables

--- Inheritance Project/src/SpaceMarine.java
+++ Inheritance Project/src/SpaceMarine.java
@@ -3,4 +3,20 @@ public class SpaceMarine
 	{
 protected int troopCount = 1000000000;
 
+public SpaceMarine()
+{
+	System.out.println("We are the defenders of humanity");
+}
+
+public void attack()
+{
+	System.out.println("Charge into battle!");
+}
+
+public void raiseBattleStandard()
+{
+	System.out.println("For humanity!");
+}
+
+
 	}
--- Inheritance Project/src/UltraMarine.java
+++ Inheritance Project/src/UltraMarine.java
@@ -1,5 +1,20 @@
 
 public class UltraMarine extends SpaceMarine
 	{
+protected int troopCount = 1000;
+
+public UltraMarine()
+{
+	System.out.println("We are the Emperor's chosen");
+}
+
+public void prepareForBattle()
+{
+	System.out.println("We will defeat the enemy with strategical advantages.");
+}
+	public void raiseBattleStandard()
+	{
+		System.out.println("For honor and the emperor!");
+	}
 
 	}
